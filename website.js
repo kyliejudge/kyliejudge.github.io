bce5d053ebedf642935a3ec4f23ea6a13133d40c
kyliejudge <128327976+kyliejudge@users.noreply.github.com>
2023-03-25 00:21:00 -0400

    Makeover 3.25
    Changed images from heic to jpegs, improved the slider on artwork, implemented a work page, and overall quality of life improvements.

--- website.js
+++ website.js
@@ -1,9 +1,47 @@
-const slider = document.querySelector('.slider');
-const images = slider.querySelectorAll('img');
-let currentImageIndex = 0;
+const sliderContainer = document.querySelector('.slider-container');
+const prevBtn = document.querySelector('.slider-prev');
+const nextBtn = document.querySelector('.slider-next');
+const slides = sliderContainer.children;
+const slideWidth = slides[0].getBoundingClientRect().width;
 
-setInterval(() => {
-  images[currentImageIndex].classList.remove('active');
-  currentImageIndex = (currentImageIndex + 1) % images.length;
-  images[currentImageIndex].classList.add('active');
-}, 5000);
+let slideIndex = 0;
+
+function setPosition() {
+  sliderContainer.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+}
+
+function resizeImages() {
+  for (let i = 0; i < slides.length; i++) {
+    const slide = slides[i];
+    const slideWidth = slide.getBoundingClientRect().width;
+    const image = slide.querySelector('img');
+    const imageWidth = image.naturalWidth;
+    const imageHeight = image.naturalHeight;
+    const aspectRatio = imageWidth / imageHeight;
+    image.style.width = `${slideWidth}px`;
+    image.style.height = `${slideWidth / aspectRatio}px`;
+  }
+}
+
+window.addEventListener('resize', resizeImages);
+
+prevBtn.addEventListener('click', () => {
+  if (slideIndex === 0) {
+    slideIndex = slides.length - 1;
+  } else {
+    slideIndex--;
+  }
+  setPosition();
+});
+
+nextBtn.addEventListener('click', () => {
+  if (slideIndex === slides.length - 1) {
+    slideIndex = 0;
+  } else {
+    slideIndex++;
+  }
+  setPosition();
+});
+
+resizeImages();
+setPosition();
